be461f2e6a2bdb4cd54368b494230cd4f1fedc97
Alessio Gilardi <alessio.gilardi91@gmail.com>
2026-06-15 20:26:11 +0200

    add new repository

--- src/guidami_ai_patente_ingestor/orchestrators/knowledge_indexing/indexing_pipeline.py
+++ src/guidami_ai_patente_ingestor/orchestrators/knowledge_indexing/indexing_pipeline.py
@@ -1,11 +1,14 @@
 import logging
 from typing import Literal
 
-from commons.clients import EmbeddingClient, VectorStoreClient
-from commons.models.knowledge import KnowledgeChunk
+from commons.clients import EmbeddingClient
+from commons.entities.knowledge import KnowledgeChunk
 from guidami_ai_patente_ingestor.configs import IngestorConfig
 from guidami_ai_patente_ingestor.entities import Article
-from guidami_ai_patente_ingestor.repositories import ArticleRepository
+from guidami_ai_patente_ingestor.repositories import (
+    ArticleRepository,
+    KnowledgeChunkStoreRepository,
+)
 from guidami_ai_patente_ingestor.services.knowledge import ArticleChunker
 
 logger = logging.getLogger(__name__)
@@ -19,14 +22,14 @@ class IndexingPipeline:
         article_repository: ArticleRepository,
         article_chunker: ArticleChunker,
         embedding_client: EmbeddingClient,
-        vector_store_client: VectorStoreClient,
+        knowledge_chunk_store_repository: KnowledgeChunkStoreRepository,
         config: IngestorConfig,
     ) -> None:
         """Inietta le dipendenze della pipeline e la configurazione."""
         self._article_repository = article_repository
         self._article_chunker = article_chunker
         self._embedding_client = embedding_client
-        self._vector_store_client = vector_store_client
+        self._knowledge_chunk_store_repository = knowledge_chunk_store_repository
         self._config = config
 
     def run(self) -> None:
@@ -46,8 +49,8 @@ class IndexingPipeline:
         self._assign_embeddings(chunks)
 
         logger.info(f"truncating knowledge_chunks, inserting {len(chunks)} chunks")
-        self._vector_store_client.truncate()
-        self._vector_store_client.bulk_insert(chunks)
+        self._knowledge_chunk_store_repository.truncate()
+        self._knowledge_chunk_store_repository.bulk_insert(chunks)
         logger.info("insertion completed")
 
     def _chunk_articles(
