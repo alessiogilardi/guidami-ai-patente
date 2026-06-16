d360935f7e840b6644a280cd265f326c9ab7e066
Alessio Gilardi <alessio.gilardi91@gmail.com>
2026-06-16 14:13:12 +0200

    increase num dim of new model

--- db/init.sql
+++ db/init.sql
@@ -14,7 +14,7 @@ CREATE TABLE IF NOT EXISTS knowledge_chunks (
     chunk_text      TEXT NOT NULL,
     is_repealed     BOOLEAN NOT NULL DEFAULT FALSE,
     source_url      TEXT NOT NULL,
-    embedding       VECTOR(384),
+    embedding       VECTOR(1536),
     UNIQUE (source, article_number, comma_index)
 );
 
