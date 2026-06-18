e5f919867c7d193b7b49c2f1712539e817855da9
Alessio Gilardi <alessio.gilardi91@gmail.com>
2026-06-18 16:00:00 +0200

    🎨

--- src/guidami_ai_patente_ingestor/entities/__init__.py
+++ src/guidami_ai_patente_ingestor/entities/__init__.py
@@ -3,4 +3,8 @@
 from .article import Article
 from .quiz_bank import QuizMainQuestion, QuizSubQuestion
 
-__all__ = ["Article", "QuizMainQuestion", "QuizSubQuestion"]
+__all__ = [
+    "Article",
+    "QuizMainQuestion",
+    "QuizSubQuestion",
+]
