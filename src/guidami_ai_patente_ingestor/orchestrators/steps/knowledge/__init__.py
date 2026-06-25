a0337b45eaf2b6c9d9540dac89b7044ff15ef202
Alessio Gilardi <alessio.gilardi91@gmail.com>
2026-06-25 15:08:24 +0200

    🔥 Remove ContextualizeStep

--- src/guidami_ai_patente_ingestor/orchestrators/steps/knowledge/__init__.py
+++ src/guidami_ai_patente_ingestor/orchestrators/steps/knowledge/__init__.py
@@ -1,13 +1,11 @@
 """Step flowstep per il dominio knowledge (corpus normativo)."""
 
 from .chunk_articles_step import ChunkArticlesStep
-from .contextualize_step import ContextualizeStep
 from .embed_chunks_step import EmbedChunksStep
 from .store_chunks_step import StoreChunksStep
 
 __all__ = [
     "ChunkArticlesStep",
-    "ContextualizeStep",
     "EmbedChunksStep",
     "StoreChunksStep",
 ]
