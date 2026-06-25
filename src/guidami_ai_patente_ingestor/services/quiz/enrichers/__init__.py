e22702f4d7974f0399fd3e94da7950be6178530a
Alessio Gilardi <alessio.gilardi91@gmail.com>
2026-06-25 12:24:05 +0200

    🔥 Remove QuizEnricher re-export from enrichers __init__

--- src/guidami_ai_patente_ingestor/services/quiz/enrichers/__init__.py
+++ src/guidami_ai_patente_ingestor/services/quiz/enrichers/__init__.py
@@ -1,6 +1,5 @@
 """Enricher del quiz bank (Open/Closed: nuovo enricher = nuova classe)."""
 
 from .image_description_enricher import ImageDescriptionEnricher
-from .quiz_enricher import QuizEnricher
 
-__all__ = ["ImageDescriptionEnricher", "QuizEnricher"]
+__all__ = ["ImageDescriptionEnricher"]
